19002f4bf9e3f81838dfd4abb00e21679ff2ac68
3MadoViski <3MadoViski@El-Prof>
2015-08-17 03:30:47 +0300

    I've removed the test class.

--- src/SearchCompany.java
+++ src/SearchCompany.java
@@ -229,7 +229,7 @@ public class SearchCompany extends javax.swing.JFrame {
         } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(SearchCompany.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.searchcompLblgging.Logger.getLogger(SearchCompany.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SearchCompany.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(SearchCompany.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
